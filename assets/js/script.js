ddbf0f2207a6d1c846bceea79dcf21442f6ccafe
Camarco <camarcovincent@gmail.com>
2024-04-17 20:38:37 -0400

    removed redundant files, added png again, disabled map zoom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //default map position
 let map = L.map('map').setView([40, -95], 4);
-
+map.doubleClickZoom.disable();
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
